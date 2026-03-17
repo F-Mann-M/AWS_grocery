4a5c668bbd79e7046e1a46b96a028c958eccbb02
Friedemann <friedemann.muecket@gmx.de>
2026-03-17 06:22:57 +0100

    Adjust app port mapping and SG ingress
    Update deployment workflow to map host port 80 to container port 5000 (changed -p 5000:5000 to -p 80:5000). Modify Terraform EC2 security group ingress

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -145,7 +145,7 @@ resource "aws_security_group" "ec2_sg" {
 
   # inbound app (Port 5000 for GroceryMate)
   ingress {
-    from_port   = 80
+    from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
